8cd6a9db4e09982d4819bb7c48ab61482c4892fc
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-05-01 15:11:04 +0530

    custom exception if extension was wrong

--- src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
@@ -1,7 +1,5 @@
 package com.bridgelabz.indianstatecensus;
 
-
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -15,12 +13,16 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 public class IndianStateCensusAnalyzer
 {
-	private static final String FILE_PATH = "D:\\\\IndianStateCensusAnalyzer_Day29\\\\src\\\\Resource";
+	private static final String FILE_PATH = "D:\\IndianStateCensusAnalyzer_Day29\\src\\Resource";
 
 	public List<StateCences> readInIndiaStateCensusData(String FileName) 
 	{
 		try 
 		{
+			if (!FileName.split("\\.")[1].equals("csv"))
+			{
+				throw new CustomExceptionService(ExceptionType.WRONG_FILE_TYPE,"enter proper extension");
+			}
 			Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH+FileName));   //reader to read contacts
 			CsvToBean<StateCences> csvToBean = new CsvToBeanBuilder<StateCences>(reader)
 					.withType(StateCences.class)
@@ -39,3 +41,4 @@ public class IndianStateCensusAnalyzer
 		return null;
 	}
 }
+
